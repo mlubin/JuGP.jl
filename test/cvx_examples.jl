739afa6ade671fb9264eea30ea3a599bbe935614
Chris Coey <coey.chris@gmail.com>
2018-04-26 19:56:43 -0400

    update copyright

--- test/cvx_examples.jl
+++ test/cvx_examples.jl
@@ -1,3 +1,7 @@
+#  Copyright 2018, Miles Lubin, Chris Coey, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # These test cases are translated from the CVX example library
 # cvxr.com/cvx/examples/
