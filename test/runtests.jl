379c182b7011dd22ebb3008fb49d97d394a9af78
Chris Coey <coey.chris@gmail.com>
2018-04-26 17:41:04 -0400

    put back lse method in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ cont_solvers = Dict()
 cont_solvers[:LogSumExp] = []
 cont_solvers[:Conic] = []
 
-# ipt && push!(cont_solvers[:LogSumExp], Ipopt.IpoptSolver(print_level=0))
+ipt && push!(cont_solvers[:LogSumExp], Ipopt.IpoptSolver(print_level=0))
 eco && push!(cont_solvers[:Conic], ECOS.ECOSSolver(verbose=false))
 
 include("operators.jl")
